3c3cd76e1a19cbccf57c305e2ef846c869f1759a
Kyle Grunwald <kyle.grunwald@miesystems.net>
2020-11-23 21:59:19 -0700

    Remove app file

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage'
+import AccountPage from './pages/AccountPage';
+
 import 'antd/dist/antd.css';
+
+
+const App = () => {
+    return (
+        <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/account" component={AccountPage} />
+        </Switch>
+    )
+}
 
 
 ReactDOM.render(
